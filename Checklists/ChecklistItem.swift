090aec3758fcec9762e86f1ad4d63c4659ee0116
Rachana Acharya <rasnacharya@gmail.com>
2015-06-15 15:32:28 -0600

    Added a ChecklistItem object to combine text and checkmark state

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Rachana Acharya on 6/15/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import Foundation
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
