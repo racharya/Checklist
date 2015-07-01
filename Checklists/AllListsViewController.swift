412dc61a7306313181d94fa5d00f4d118317a8d1
Rachana Acharya <rasnacharya@gmail.com>
2015-07-01 01:31:56 -0600

    Added logic to create the cells in code itself

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -29,7 +29,17 @@ class AllListsViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+        if cell == nil {
+            cell = UITableViewCell(style:.Default, reuseIdentifier: cellIdentifier)
+        }
+        cell.textLabel!.text = "List \(indexPath.row)"
+        return cell
     }
 
     /*
