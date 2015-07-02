5c267ca4bc1908638862a394796f206f9e6ae4a5
Rachana Acharya <rasnacharya@gmail.com>
2015-07-01 20:08:11 -0600

    Added a new file to let users add new checklists

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,18 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Rachana Acharya on 7/1/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+  var name = " "
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
