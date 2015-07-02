060c42d0d14bb6247d37ad5bf89ee84db4bbec25
Rachana Acharya <rasnacharya@gmail.com>
2015-07-01 22:36:05 -0600

    Added logic to include items in the AllItems scene

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -9,52 +9,49 @@
 import UIKit
 
 class AllListsViewController: UITableViewController {
-    var lists: [Checklist] // array to hold checklist objects
+    
+    var lists:[Checklist]
     
     required init(coder aDecoder: NSCoder) {
-    //1: Give lists variable a value
-    lists = [Checklist]()
-    //2: Call super version of init(coder)
-    super.init(coder: aDecoder)
-    //3: Create a new Checklist object, give name , add to array
-    var list = Checklist()
-    list.name = "Birthdays"
-    lists.append(list)
-    
-    //4
-    list = Checklist()
-    list.name = "Groceries"
-    lists.append(list)
-    
-    list = Checklist()
-    list.name = "Cool Apps"
-    lists.append(list)
-    
-    list = Checklist()
-    list.name = "To Do"
-    lists.append(list)
-    
-}
+        lists = [Checklist]() // array to hold checklist objects
+      
+        //2: Call super version of init(coder)
+        super.init(coder: aDecoder)
+        //3: Create a new Checklist object, give name , add to array
+        var list = Checklist(name: "Birthdays")
+        lists.append(list)
+        
+        //4
+        list = Checklist(name: "Groceries")
+        lists.append(list)
+        
+        list = Checklist(name: "Cool Apps")
+        lists.append(list)
+        
+        list = Checklist(name: "To Do")
+        lists.append(list)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 3
+        return lists.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -63,14 +60,17 @@ class AllListsViewController: UITableViewController {
         if cell == nil {
             cell = UITableViewCell(style:.Default, reuseIdentifier: cellIdentifier)
         }
-        cell.textLabel!.text = "List \(indexPath.row)"
+        let checklist = lists[indexPath.row]
+        cell.textLabel!.text = checklist.name
+        cell.accessoryType = .DetailDisclosureButton
+        
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         performSegueWithIdentifier("ShowChecklist", sender: nil)
     }
-
- 
-
+    
+    
+    
 }
