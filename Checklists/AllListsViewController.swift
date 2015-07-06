8f32d9d1467d31ec83d787163e410ea9fa7207bd
Rachana Acharya <rasnacharya@gmail.com>
2015-07-05 22:10:55 -0600

    Committing all changes

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -2,104 +2,129 @@
 //  AllListsViewController.swift
 //  Checklists
 //
-//  Created by Rachana Acharya on 7/1/15.
-//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//  Created by M.I. Hollemans on 18/09/14.
+//  Copyright (c) 2014 Razeware. All rights reserved.
 //
 
 import UIKit
 
 class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
+
+  var lists: [Checklist]
+
+  required init(coder aDecoder: NSCoder) {
+    lists = [Checklist]()
     
-    var lists:[Checklist]
+    super.init(coder: aDecoder)
     
-    required init(coder aDecoder: NSCoder) {
-        lists = [Checklist]() // array to hold checklist objects
-        
-        //2: Call super version of init(coder)
-        super.init(coder: aDecoder)
-        //3: Create a new Checklist object, give name , add to array
-        var list = Checklist(name: "Birthdays")
-        lists.append(list)
-        
-        //4
-        list = Checklist(name: "Groceries")
-        lists.append(list)
-        
-        list = Checklist(name: "Cool Apps")
-        lists.append(list)
-        
-        list = Checklist(name: "To Do")
-        lists.append(list)
-        
+    var list = Checklist(name: "Birthdays")
+    lists.append(list)
+    
+    list = Checklist(name: "Groceries")
+    lists.append(list)
+    
+    list = Checklist(name: "Cool Apps")
+    lists.append(list)
+    
+    list = Checklist(name: "To Do")
+    lists.append(list)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  // MARK: - Table view data source
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return lists.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      
+    let cellIdentifier = "Cell"
+    var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+    if cell == nil {
+      cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    let checklist = lists[indexPath.row]
+    cell.textLabel!.text = checklist.name
+    cell.accessoryType = .DetailDisclosureButton
+    
+    return cell
+  }
+
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    let checklist = lists[indexPath.row]
+    performSegueWithIdentifier("ShowChecklist", sender: checklist)
+  }
+
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle,forRowAtIndexPath indexPath: NSIndexPath) {
+    lists.removeAtIndex(indexPath.row)
+    
+    let indexPaths = [indexPath]
+    tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+  }
+
+  override func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+    let navigationController = storyboard!.instantiateViewControllerWithIdentifier("ListNavigationController") as! UINavigationController
+    
+    let controller = navigationController.topViewController as! ListDetailViewController
+    controller.delegate = self
+    
+    let checklist = lists[indexPath.row]
+    controller.checklistToEdit = checklist
+    
+    presentViewController(navigationController, animated: true, completion: nil)
+  }
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "ShowChecklist" {
+      let controller = segue.destinationViewController as! ChecklistViewController
+      controller.checklist = sender as! Checklist
+    } else if segue.identifier == "AddChecklist" {
+      let navigationController = segue.destinationViewController as! UINavigationController
+      let controller = navigationController.topViewController as! ListDetailViewController
+      controller.delegate = self
+      controller.checklistToEdit = nil
     }
+  }
+  
+  func listDetailViewControllerDidCancel(controller: ListDetailViewController) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func listDetailViewController(controller: ListDetailViewController, didFinishAddingChecklist checklist: Checklist) {
+    let newRowIndex = lists.count
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    lists.append(checklist)
     
+    let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
+    let indexPaths = [indexPath]
+    tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return lists.count
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cellIdentifier = "Cell"
-        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
-        if cell == nil {
-            cell = UITableViewCell(style:.Default, reuseIdentifier: cellIdentifier)
-        }
-        let checklist = lists[indexPath.row]
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func listDetailViewController(controller: ListDetailViewController, didFinishEditingChecklist checklist: Checklist) {
+    if let index = find(lists, checklist) {
+      let indexPath = NSIndexPath(forRow: index, inSection: 0)
+      if let cell = tableView.cellForRowAtIndexPath(indexPath) {
         cell.textLabel!.text = checklist.name
-        cell.accessoryType = .DetailDisclosureButton
-        
-        return cell
+      }
     }
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let checklist = lists[indexPath.row]
-        performSegueWithIdentifier("ShowChecklist", sender: checklist)
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "ShowChecklist" {
-            let controller = segue.destinationViewController as! ChecklistViewController
-            controller.checklist = sender as! Checklist
-            
-        } else if segue.identifier == "AddChecklist" {
-            let navigationController = segue.destinationViewController as! UINavigationController
-            let controller = navigationController.topViewController as! ListDetailViewController
-            controller.delegate = self
-            controller.checklistToEdit = nil
-        }
-    }
-   
-    func listDetailViewControllerDidCancel(controller: ListDetailViewController) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func listDetailViewController(controller: ListDetailViewController, didFinishAddingChecklist checklist: Checklist) {
-        let newRowIndex = lists.count
-        lists.append(checklist)
-        
-        let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
-        let indexPaths = [indexPath]
-        tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func listDetailViewController(controller: ListDetailViewController, didFinishEditingChecklist checklist: Checklist) {
-        if let index = find(lists, checklist) {
-            let indexPath = NSIndexPath(forRow: index, inSection: 0)
-            if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-                cell.textLabel!.text = checklist.name
-            }
-        }
-        dismissViewControllerAnimated(true, completion: nil)
-    }
+    dismissViewControllerAnimated(true, completion: nil)
+  }
 }
--- Checklists/ItemDetailViewController.swift
+++ Checklists/ItemDetailViewController.swift
@@ -2,74 +2,70 @@
 //  ItemDetailViewController.swift
 //  Checklists
 //
-//  Created by Rachana Acharya on 6/22/15.
-//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//  Created by M.I. Hollemans on 16/09/14.
+//  Copyright (c) 2014 Razeware. All rights reserved.
 //
 
 import UIKit
 
 protocol ItemDetailViewControllerDelegate: class {
-    func itemDetailViewControllerDidCancel(controller: ItemDetailViewController)
-    func itemDetailViewController(controller: ItemDetailViewController, didFinishAddingItem item: ChecklistItem)
-    func itemDetailViewController(controller: ItemDetailViewController, didFinishEditingItem item: ChecklistItem)
-    
+  func itemDetailViewControllerDidCancel(controller: ItemDetailViewController)
+  func itemDetailViewController(controller: ItemDetailViewController, didFinishAddingItem item: ChecklistItem)
+  func itemDetailViewController(controller: ItemDetailViewController, didFinishEditingItem item: ChecklistItem)
 }
 
 class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var doneBarButton: UIBarButtonItem!
-    weak var delegate: ItemDetailViewControllerDelegate?
-    var itemToEdit: ChecklistItem?
-    
-    @IBAction func cancel(){
-        delegate?.itemDetailViewControllerDidCancel(self) // does not send message if delegate is nil
-        //dismissViewControllerAnimated(true, completion: nil)
+
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
+
+  weak var delegate: ItemDetailViewControllerDelegate?
+
+  var itemToEdit: ChecklistItem?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.rowHeight = 44
+
+    if let item = itemToEdit {
+      title = "Edit Item"
+      textField.text = item.text
+      doneBarButton.enabled = true
     }
-    
-    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        
-        let oldText: NSString = textField.text // new string in the text field
-        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
-        
-        doneBarButton.enabled = (newText.length > 0)
-        return true
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+
+  @IBAction func cancel() {
+    delegate?.itemDetailViewControllerDidCancel(self)
+  }
+
+  @IBAction func done() {
+    if let item = itemToEdit {
+      item.text = textField.text
+      delegate?.itemDetailViewController(self, didFinishEditingItem: item)
+      
+    } else {
+      let item = ChecklistItem()
+      item.text = textField.text
+      item.checked = false
+      delegate?.itemDetailViewController(self, didFinishAddingItem: item)
     }
-    
-    @IBAction func done(){
-        //        println("Contents of the text field: \(textField.text)")
-        //        dismissViewControllerAnimated(true, completion: nil)
-        if let item = itemToEdit {// unwrapping an optional
-            item.text = textField.text
-            delegate?.itemDetailViewController(self, didFinishEditingItem: item)
-            
-        } else {
-            let item = ChecklistItem()
-            item.text = textField.text
-            item.checked = false
-            delegate?.itemDetailViewController(self, didFinishAddingItem: item)//doesnot send message if delegate is nil
-        }
-    }
-    
-    // ? says tells swift that you can return nil from this method. Only allowed if ? behind that return type
-    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-        return nil // nil means no value | nothing found | don't do anything etc
-    }
-    
-    //automatically allows keyboard to appear once the Add Item screen opens
-    override func viewWillAppear(animated: Bool){
-        super.viewWillAppear(animated)
-        textField.becomeFirstResponder()
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.rowHeight = 44
-        
-        if let item = itemToEdit {
-            title = "Edit Item"
-            textField.text = item.text
-            doneBarButton.enabled = true
-        }
-    }
+  }
+
+  override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+    return nil
+  }
+
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+      
+    let oldText: NSString = textField.text
+    let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+
+    doneBarButton.enabled = (newText.length > 0)
+    return true
+  }
 }
