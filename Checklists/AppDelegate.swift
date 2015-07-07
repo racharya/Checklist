22732da225f8c4949ee1bc5e4860ab85c9f27a9b
Rachana Acharya <rasnacharya@gmail.com>
2015-07-06 23:19:57 -0600

    Called saveData() method as  needed

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -27,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        saveData()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -39,12 +40,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        saveData()
     }
     // trying to find all list view controller from within app delegate
     // goal is to saveData when the app terminates
     func saveData() {
-        //window! means force unwrap 
+        //window! means force unwrap
         let navigationController = window!.rootViewController as! UINavigationController
+        //
         let controller = navigationController.viewControllers[0] as! AllListsViewController
         controller.saveChecklists()
     }
