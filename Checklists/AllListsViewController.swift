c38f7168ce78b4accf75b8330fbfce9b06428d94
Rachana Acharya <rasnacharya@gmail.com>
2015-07-06 21:13:14 -0600

    Added logic to loading and saving lists

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -136,6 +136,31 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         }
         dismissViewControllerAnimated(true, completion: nil)
     }
-   
-   
+   //load/save code
+    func documentsDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true) as! [String]
+        return paths[0]
+    }
+    func dataFilePath() -> String {
+        return documentsDirectory().stringByAppendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        archiver.encodeObject(lists, forKey: "Checklists")
+        archiver.finishEncoding()
+        data.writeToFile(dataFilePath(), atomically: true)
+    }
+    
+    func loadChecklists() {
+        let path = dataFilePath()
+        if NSFileManager.defaultManager().fileExistsAtPath(path) {
+            if let data = NSData(contentsOfFile: path){
+            let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+            lists = unarchiver.decodeObjectForKey("Checklists") as! [Checklist]
+            unarchiver.finishDecoding()
+        }
+        }
+    }// end of load/save code
 }
