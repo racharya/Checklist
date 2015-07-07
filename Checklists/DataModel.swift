761ef61ea36e0b180940bdfbdfd5c119eef1cf5c
Rachana Acharya <rasnacharya@gmail.com>
2015-07-06 23:49:53 -0600

    Pasted all the methods removed from AllListsViewController

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -8,6 +8,38 @@
 
 import Foundation
 
+// takes over the responsibilities of loading and saving the to-do lists from AllListsViewController
 class DataModel {
     var lists = [Checklist]()
+    
+    init() { //as soon as DataModel object is created, it will attempt to load Checklists.plist
+        loadChecklists()
+    }
+    //load/save code
+    func documentsDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true) as! [String]
+        return paths[0]
+    }
+    func dataFilePath() -> String {
+        return documentsDirectory().stringByAppendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        archiver.encodeObject(lists, forKey: "Checklists")
+        archiver.finishEncoding()
+        data.writeToFile(dataFilePath(), atomically: true)
+    }
+    
+    func loadChecklists() {
+        let path = dataFilePath()
+        if NSFileManager.defaultManager().fileExistsAtPath(path) {
+            if let data = NSData(contentsOfFile: path){
+                let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+                lists = unarchiver.decodeObjectForKey("Checklists") as! [Checklist]
+                unarchiver.finishDecoding()
+            }
+        }
+    }// end of load/save code
 }
