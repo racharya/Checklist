3eb0ca44260033b38481302da7a9c32cd3be50aa
Rachana Acharya <rasnacharya@gmail.com>
2015-07-06 23:36:42 -0600

    Adding a new file to represent a top level data model

---
+++ Checklists/DataModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataModel.swift
+//  Checklists
+//
+//  Created by Rachana Acharya on 7/6/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import Foundation
+
+class DataModel {
+    var lists = [Checklist]()
+}
