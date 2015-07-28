606e88e25059b0c1c222b961a280f45c05498fba
Rachana Acharya <rasnacharya@gmail.com>
2015-07-27 23:15:45 -0600

    Initialized iconName to No Image

--- Checklists/Checklist.swift
+++ Checklists/Checklist.swift
@@ -15,7 +15,7 @@ class Checklist: NSObject, NSCoding {
     
     init(name: String) {
         self.name = name
-        iconName = "Appointments"
+        iconName = "No Icon"
         super.init()
     }
     
