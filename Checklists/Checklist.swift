7dc167783fadee43c54c95e0a1629f6fe6bf0211
Rachana Acharya <rasnacharya@gmail.com>
2015-07-27 22:54:47 -0600

    Added a property to hold filename of the icon image, added lines to use that property

--- Checklists/Checklist.swift
+++ Checklists/Checklist.swift
@@ -9,23 +9,27 @@
 import UIKit
 
 class Checklist: NSObject, NSCoding {
-  var name = " "
+    var name = " "
     var items = [ChecklistItem]()
+    var iconName: String
     
     init(name: String) {
         self.name = name
+        iconName = "Appointments"
         super.init()
     }
     
     required init(coder aDecoder: NSCoder) {
         name = aDecoder.decodeObjectForKey("Name") as! String
         items = aDecoder.decodeObjectForKey("Items") as! [ChecklistItem]
+        iconName = aDecoder.decodeObjectForKey("IconName") as! String
         super.init()
     }
     
     func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(name, forKey: "Name")
-        aCoder.encodeObject(items, forKey:"Items")
+        aCoder.encodeObject(items, forKey: "Items")
+        aCoder.encodeObject(iconName, forKey: "IconName")
     }
     
     func countUncheckedItems() -> Int {
