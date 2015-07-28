fc52238dc339d66cf0a820284866f1ac0be7294b
Rachana Acharya <rasnacharya@gmail.com>
2015-07-27 23:14:02 -0600

    New view controller or icon picker screen

---
+++ IconPickerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  IconPickerViewController.swift
+//  Checklists
+//
+//  Created by Rachana Acharya on 7/27/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import UIKit
+
+protocol IconPickerViewControllerDelegate: class {
+    func iconPicker(picker: IconPickerViewController, didPickIcon iconName: String)
+}
+
+class IconPickerViewController: UITableViewController {
+    weak var delegate: IconPickerViewControllerDelegate?
+    
+    let icons = ["No Icon", "Appointments", "Birthdays", "Chores", "Drin", "Folder", "Groceries", "Inbox", "Photos", "Trips"]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    override func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("IconCell") as! UITableViewCell
+        let iconName = icons[indexPath.row]
+        cell.textLabel!.text = iconName
+        cell.imageView!.image = UIImage(named: iconName)
+        return cell
+    }
+}
+
+
