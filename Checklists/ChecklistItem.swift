c7a851d384ff25bfa2fd9222e4580f3f8d3d49ec
Rachana Acharya <rasnacharya@gmail.com>
2015-07-30 16:41:30 -0600

    Added few instance variables; logic to assign unique id to each checklists

--- Checklists/ChecklistItem.swift
+++ Checklists/ChecklistItem.swift
@@ -12,11 +12,15 @@ class ChecklistItem: NSObject, NSCoding {
     var text = ""
     var checked = false
     
-    init(text: String, checked: Bool) {
-        self.text = text
-        self.checked = checked
-        super.init()
-    }
+    var dueDate = NSDate()
+    var shouldRemind = false
+    var itemID: Int
+//    
+//    init(text: String, checked: Bool) {
+//        self.text = text
+//        self.checked = checked
+//        super.init()
+//    }
     
     func toggleChecked() {
         checked = !checked
@@ -25,17 +29,26 @@ class ChecklistItem: NSObject, NSCoding {
     func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(text, forKey: "Text")
         aCoder.encodeBool(checked, forKey: "Checked")
+        aCoder.encodeObject(dueDate, forKey: "DueDate")
+        aCoder.encodeBool(shouldRemind, forKey: "shouldRemind")
+        aCoder.encodeInteger(itemID, forKey: "itemID")
     }
     
     //needed to make the app compile without errors
     required init(coder aDecoder: NSCoder) {
         text = aDecoder.decodeObjectForKey("Text") as! String
         checked = aDecoder.decodeBoolForKey("Checked")
+        dueDate = aDecoder.decodeObjectForKey("DueDate") as! NSDate
+        shouldRemind = aDecoder.decodeBoolForKey("shouldRemind")
+        itemID = aDecoder.decodeIntegerForKey("ItemID")
         super.init()
     }
     
     //needed to build the app, keeps compiler happy
     override init() {
+        itemID = DataModel.nextChecklistItemID()
         super.init()
     }
+    
+    
 }
