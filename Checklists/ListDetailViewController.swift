0aa034fb5b66fff46f723cc52f476fe93c9a0a87
Rachana Acharya <rasnacharya@gmail.com>
2015-07-30 00:08:04 -0600

    Created checklist using new init method

--- Checklists/ListDetailViewController.swift
+++ Checklists/ListDetailViewController.swift
@@ -53,8 +53,8 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate, Icon
             checklist.iconName = iconName
             delegate?.listDetailViewController(self, didFinishEditingChecklist: checklist)
         } else {
-            let checklist = Checklist(name: textField.text)
-            checklist.iconName = iconName
+            let checklist = Checklist(name: textField.text, iconName: iconName)
+            
             delegate?.listDetailViewController(self, didFinishAddingChecklist: checklist)
         }
     }
