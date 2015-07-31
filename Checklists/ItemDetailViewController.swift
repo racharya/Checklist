86500940be49721456de0585ae2508634bc15426
Rachana Acharya <rasnacharya@gmail.com>
2015-07-30 21:32:35 -0600

    Added tableView methods for row height, number of rows in section etc

--- Checklists/ItemDetailViewController.swift
+++ Checklists/ItemDetailViewController.swift
@@ -27,6 +27,7 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     
     var itemToEdit: ChecklistItem?
     var dueDate = NSDate()
+    var datePickerVisible = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -85,5 +86,58 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
         
         doneBarButton.enabled = (newText.length > 0)
         return true
+    }
+    /* sets the datePickerVisible to true, and then tells the table view to insert a 
+    new row below the Due Date cell. This new row will contain the UIDatePicker component */
+    func showDatePicker() {
+        datePickerVisible = true
+        let indexPathDatePicker = NSIndexPath(forRow: 2, inSection: 1)
+        tableView.insertRowsAtIndexPaths([indexPathDatePicker], withRowAnimation: .Fade)
+    }
+    
+    //overriding the data source for a static table view 
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        //1: check whether this is the indexpath for the row with the date picker
+        //IF NOT--> jump to step 5
+        if indexPath.section == 1 && indexPath.row == 2 {
+            
+            //2: Ask table view if it already has date picker cell, IF NOT create a new one
+            var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier("DatePickerCell") as? UITableViewCell
+            if cell == nil {
+                cell == UITableViewCell(style: .Default, reuseIdentifier: "DatePickerCell")
+                cell.selectionStyle = .None
+                
+                //3: create a new UIDatePicker component, tag 100
+                let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: 320, height: 216))
+                datePicker.tag = 100
+                cell.contentView.addSubview(datePicker)
+                
+                //4: tell datePicker to call dateChanged method everytime user picks a new date
+                datePicker.addTarget(self, action: Selector("dateChanged"), forControlEvents:.ValueChanged)
+            }
+            return cell
+            
+            //5: if not datepicker cell than call super so other static cell functions as it is
+        } else {
+            return super.tableView(tableView,cellForRowAtIndexPath: indexPath)
+        }
+    }
+    
+    //if datepicker is visible then section 1 has 3 rows, if not simply pass through to the original data source
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 1 && datePickerVisible {
+            return 3
+        } else {
+            return super.tableView(tableView, numberOfRowsInSection: section)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == 2 {
+            return 217
+        } else {
+            return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+        }
     }
 }
