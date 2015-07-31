236e53a2a801a67ee8e3eb38da22065cf7ef0f67
Rachana Acharya <rasnacharya@gmail.com>
2015-07-30 20:14:12 -0600

    Added logic to convert NSDate value to text

--- Checklists/ItemDetailViewController.swift
+++ Checklists/ItemDetailViewController.swift
@@ -9,63 +9,77 @@
 import UIKit
 
 protocol ItemDetailViewControllerDelegate: class {
-  func itemDetailViewControllerDidCancel(controller: ItemDetailViewController)
-  func itemDetailViewController(controller: ItemDetailViewController, didFinishAddingItem item: ChecklistItem)
-  func itemDetailViewController(controller: ItemDetailViewController, didFinishEditingItem item: ChecklistItem)
+    func itemDetailViewControllerDidCancel(controller: ItemDetailViewController)
+    func itemDetailViewController(controller: ItemDetailViewController, didFinishAddingItem item: ChecklistItem)
+    func itemDetailViewController(controller: ItemDetailViewController, didFinishEditingItem item: ChecklistItem)
 }
 
 class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
-
-  @IBOutlet weak var textField: UITextField!
-  @IBOutlet weak var doneBarButton: UIBarButtonItem!
-
-  weak var delegate: ItemDetailViewControllerDelegate?
-
-  var itemToEdit: ChecklistItem?
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.rowHeight = 44
-
-    if let item = itemToEdit {
-      title = "Edit Item"
-      textField.text = item.text
-      doneBarButton.enabled = true
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var shouldRemindSwitch: UISwitch!
+    @IBOutlet weak var dueDateLabel: UILabel!
+    
+    
+    weak var delegate: ItemDetailViewControllerDelegate?
+    
+    var itemToEdit: ChecklistItem?
+    var dueDate = NSDate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 44
+        
+        if let item = itemToEdit {
+            title = "Edit Item"
+            textField.text = item.text
+            doneBarButton.enabled = true
+            shouldRemindSwitch.on = item.shouldRemind
+            dueDate = item.dueDate
+        }
+        updateDueDateLabel()
     }
-  }
-  
-  override func viewWillAppear(animated: Bool) {
-    super.viewWillAppear(animated)
-    textField.becomeFirstResponder()
-  }
-
-  @IBAction func cancel() {
-    delegate?.itemDetailViewControllerDidCancel(self)
-  }
-
-  @IBAction func done() {
-    if let item = itemToEdit {
-      item.text = textField.text
-      delegate?.itemDetailViewController(self, didFinishEditingItem: item)
-      
-    } else {
-      let item = ChecklistItem()
-      item.text = textField.text
-      item.checked = false
-      delegate?.itemDetailViewController(self, didFinishAddingItem: item)
+    
+    func updateDueDateLabel() {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .ShortStyle
+        dueDateLabel.text = formatter.stringFromDate(dueDate)
     }
-  }
-
-  override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-    return nil
-  }
-
-  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-      
-    let oldText: NSString = textField.text
-    let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
-
-    doneBarButton.enabled = (newText.length > 0)
-    return true
-  }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    @IBAction func cancel() {
+        delegate?.itemDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        if let item = itemToEdit {
+            item.text = textField.text
+            delegate?.itemDetailViewController(self, didFinishEditingItem: item)
+            
+        } else {
+            let item = ChecklistItem()
+            item.text = textField.text
+            item.checked = false
+            delegate?.itemDetailViewController(self, didFinishAddingItem: item)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText: NSString = textField.text
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        doneBarButton.enabled = (newText.length > 0)
+        return true
+    }
 }
